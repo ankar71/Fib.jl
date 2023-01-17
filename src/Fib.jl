8a0310e4e8131a6c8f3063c4bf49c3e619c1c5e1
AK <ankar71gr@outlook.com>
2023-01-17 21:32:57 +0200

    First commit

--- src/Fib.jl
+++ src/Fib.jl
@@ -1,5 +1,29 @@
 module Fib
 
-# Write your package code here.
+function fib(n::Unsigned)
+    if n ≤ 1
+        return BigInt(n)
+    end
+    f0 = BigInt(0)
+    f1 = BigInt(1)
+    for i = 2:n
+        f0, f1 = f1, f0 + f1
+    end
+    f1
+end
+
+function fib(n::Signed)
+    try
+        fib(convert(Unsigned, n))
+    catch e
+        if isa(e, InexactError)
+            throw(DomainError(n))
+        else
+            rethrow(e)
+        end
+    end
+end
+
+export fib
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,16 @@ using Fib
 using Test
 
 @testset "Fib.jl" begin
-    # Write your tests here.
+    @test fib(0x0)    == 0
+    @test fib(0x0001) == 1
+    @test fib(0x03)   == 2
+    @test fib(0x0A)   == 55
+
+
+    @test fib(0)  == 0
+    @test fib(1)  == 1
+    @test fib(3)  == 2
+    @test fib(10) == 55
+
+    @test_throws DomainError fib(-1)
 end
