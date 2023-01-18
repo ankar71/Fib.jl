635548fe375ede39ae04c9a9c038f2fae60d1ca5
AK <ankar71gr@outlook.com>
2023-01-19 00:14:14 +0200

    Restrict next_fib_pair to BigInt

--- src/Fib.jl
+++ src/Fib.jl
@@ -2,7 +2,7 @@ module Fib
 export fib, fib_seq, main
 
 const first_fib_pair = BigInt(0), BigInt(1)
-@inline next_fib_pair(f0, f1) = f1, f0 + f1
+@inline next_fib_pair(f0::BigInt, f1::BigInt) = f1, f0 + f1
 
 include("Num.jl")
 using .Num
