7742935b994cb6efc7c727c4b27f24c102081608
AK <ankar71gr@outlook.com>
2023-01-18 13:47:16 +0200

    separate fib_seq tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Fib
 using Test
 
-@testset "Fib.jl" begin
+@testset "Test Fib.fib" begin
     @test fib(0x0)    == 0
     @test fib(0x0001) == 1
     @test fib(0x03)   == 2
@@ -15,18 +15,13 @@ using Test
 
     @test_throws DomainError fib(-1)
 
+end
 
-    # basic sequence
-    fs = fib_seq(0x0A)
-    @test fs[0] == fib(0)
-    @test fs[1] == fib(1)
-    @test fs[2] == fib(2)
-    @test fs[5] == fib(5)
-
-    # various sequences
+@testset "Test Fib.fib_seq" begin
+    @test fib_seq(0x0A)[0:10] == BigInt[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     @test fib_seq(0x0A) == fib_seq(10)
     @test_throws DomainError fib_seq(-1)
-    @test fib_seq(0x2:0x5) == [BigInt(1), 2, 3, 5]
+    @test fib_seq(0x2:0x5) == BigInt[1, 2, 3, 5]
     @test fib_seq(0x2:0x5) == fib_seq(2:5)
     @test_throws DomainError fib_seq(-2:5)
 
